e2da674ca72076d9279461ecbefbc342e54b43ff
dmoore <dmoore>
2003-02-14 18:10:09 +0000

    *** empty log message ***

--- ACMEPy/society.py
+++ ACMEPy/society.py
@@ -106,7 +106,6 @@ class Society:
 	print "\t\t", theNode
 	for agent in self.hosts[host].nodes[node].agents.keys():
 	  print "\t\t\t", theNode.agents[agent]
-	  for component in agent.components:
-	    theComponent = agent.components[component]
-	    print "\t\t\t\t", theComponent
+	  for component in theNode.agents[agent].components:
+	    print "\t\t\t\t", component
 

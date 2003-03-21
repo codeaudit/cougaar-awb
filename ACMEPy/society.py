8ceac4313251f990cd710c13c3cad6a8d1f9684c
pgardella <pgardella>
2003-03-21 21:49:13 +0000

    Added a toString() method that returns the elements in the society in order.  Similar to prettyFormat() except the latter iterates over a dictionary to return society elements, which doesn't guarantee any specific ordering of returned elements.  If you need a specific ordering, use toString().
    Also added a get_host() method.

--- ACMEPy/society.py
+++ ACMEPy/society.py
@@ -40,6 +40,7 @@ class Society:
     self.controller = None
     self.hostlist = []
     self.rule = str(rule)
+    
   def __str__(self):
     return "Society:"+ self.name+":RULE:"+self.rule
     
@@ -56,6 +57,9 @@ class Society:
       self.hostlist.append(h) # (host)      
       self.hosts[host].society = self
       return self.hosts[host]
+    
+  def get_host(self, index):
+    return self.hostlist[index]
 
   def set_rule(self, newRule):
         self.rule = str(newRule)
@@ -64,7 +68,7 @@ class Society:
     actives = []
     for host in self.hosts.keys():
       if len(hosts.nodes) > 0: 
-	actives.append(host.clone)
+        actives.append(host.clone)
     return actives 
 
   def each_host(self):
@@ -74,20 +78,20 @@ class Society:
   def each_node(self):
     for host in self.each_host():
       for node in host.each_node():
-	yield node
+        yield node
 
   def each_agent(self):
     for host in self.each_host():
       for node in host.each_node():
-	for agent in node.each_agent():
-	  yield agent
+        for agent in node.each_agent():
+          yield agent
 
   def each_component(self):
     for host in self.each_host():
       for node in host.each_node():
-	for agent in node.each_agent():
-	  for component in agent.each_component():
-	    yield component
+        for agent in node.each_agent():
+          for component in agent.each_component():
+            yield component
 
 
   def to_xml(self):
@@ -117,26 +121,40 @@ class Society:
     for host in self.hosts.keys():
       print "\t", self.hosts[host]
       for node in self.hosts[host].nodes.keys():
-	theNode = self.hosts[host].nodes[node]
-	print "\t\t", theNode
-	for agent in self.hosts[host].nodes[node].agents.keys():
-	  print "\t\t\t", theNode.agents[agent]
-	  for component in theNode.agents[agent].components:
-	    print "\t\t\t\t", component
-	    
+        theNode = self.hosts[host].nodes[node]
+        print "\t\t", theNode
+        for agent in self.hosts[host].nodes[node].agents.keys():
+          print "\t\t\t", theNode.agents[agent]
+          for component in theNode.agents[agent].components:
+            print "\t\t\t\t", component
+    
   def prettyFormat(self):
     text = str(self)+"\n"
     for host in self.hosts.keys():
       text = text+str(self.hosts[host])+"\n"
       for node in self.hosts[host].nodes.keys():
-	theNode = self.hosts[host].nodes[node]
-	text = text+str(theNode)+"\n"
-	for agent in self.hosts[host].nodes[node].agents.keys():
-	  text = text + str(theNode.agents[agent])+"\n"
-	  for component in theNode.agents[agent].components:
-	    text = text + str(component)+"\n"
-	    for argument in component.arguments:
-	      text = text + str(argument)+"\n"
+        theNode = self.hosts[host].nodes[node]
+        text = text+str(theNode)+"\n"
+        for agent in self.hosts[host].nodes[node].agents.keys():
+          text = text + str(theNode.agents[agent])+"\n"
+          for component in theNode.agents[agent].components:
+            text = text + str(component)+"\n"
+            for argument in component.arguments:
+              text = text + str(argument)+"\n"
+    return text
+
+  def toString(self):
+    text = str(self)+"\n"
+    for host in self.hostlist:
+      text = text+str(host)+"\n"
+      for node in host.nodelist:
+        text = text+str(node)+"\n"
+        for agent in node.agentlist:
+          text = text + str(agent)+"\n"
+          for component in agent.components:
+            text = text + str(component)+"\n"
+            for argument in component.arguments:
+              text = text + str(argument)+"\n"
     return text
 
   def fromPrettyFormat(self, text):

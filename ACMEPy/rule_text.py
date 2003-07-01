f1df0ae8105d4f6a21750485eadc8c19a26b4bc1
pgardella <pgardella>
2003-07-01 16:15:37 +0000

    Added copyright stuff.

--- ACMEPy/rule_text.py
+++ ACMEPy/rule_text.py
@@ -1,4 +1,30 @@
-# rule_text.py
+#----------------------------------------------------------------------------
+# Name:         rule_text.py
+# Purpose:      
+#
+# Author:       ISAT (D. Moore/P. Gardella)
+#
+# RCS-ID:       $Id: rule_text.py,v 1.8 2003-07-01 16:15:37 pgardella Exp $
+#  <copyright>
+#  Copyright 2002 BBN Technologies, LLC
+#  under sponsorship of the Defense Advanced Research Projects Agency (DARPA).
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the Cougaar Open Source License as published by
+#  DARPA on the Cougaar Open Source Website (www.cougaar.org).
+#
+#  THE COUGAAR SOFTWARE AND ANY DERIVATIVE SUPPLIED BY LICENSOR IS
+#  PROVIDED 'AS IS' WITHOUT WARRANTIES OF ANY KIND, WHETHER EXPRESS OR
+#  IMPLIED, INCLUDING (BUT NOT LIMITED TO) ALL IMPLIED WARRANTIES OF
+#  MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE, AND WITHOUT
+#  ANY WARRANTIES AS TO NON-INFRINGEMENT.  IN NO EVENT SHALL COPYRIGHT
+#  HOLDER BE LIABLE FOR ANY DIRECT, SPECIAL, INDIRECT OR CONSEQUENTIAL
+#  DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE OF DATA OR PROFITS,
+#  TORTIOUS CONDUCT, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+#  PERFORMANCE OF THE COUGAAR SOFTWARE.
+# </copyright>
+#
+
 from string import *
 import os
 

816d65e9b51d5e821d541c939297985cce700544
dmoore <dmoore>
2004-03-25 21:38:25 +0000

    added a second representation type called ULHierarchy

--- ACMEPy/Ultralog/organisations.py
+++ ACMEPy/Ultralog/organisations.py
@@ -7,10 +7,16 @@ from ACMEPy.argument import Argument
 from ACMEPy.parameter import *
 
 
+
+from xml.dom import minidom 
+from Ft.Xml.Domlette import Print, PrettyPrint
+from Ft.Xml.Domlette import NonvalidatingReader
+
+import sys,os
+
 class AgentMetadata:
-    def __init__(self, orgId, agentName):
+    def __init__(self, agentName):
         self.agentName = agentName
-        self.org_id = orgId[0]
         self.subordinates = []
         self.superior = None # even tho the agent.get_fact_values() returns a list, we coerce into a single value, by ignoring all members but the zero'th
         self.level = 0
@@ -26,8 +32,8 @@ class AgentMetadata:
         return False
     def setLevel(self, level): self.level = level
     def __str__(self):
-        return "AgentMetadata:Agent:"+str(self.agentName)+" organisation: "+str(self.org_id)+" superior "+str(self.superior)+  " subordinates: "+str(self.subordinates)+"Level "+str(self.level)+"\n"
-        #~ return "AgentMetadata:Agent:"+self.agentName+"organisation:"+self.org_id+"\n"
+        return "AgentMetadata:Agent:"+str(self.agentName)+" superior "+str(self.superior)+  " subordinates: "+str(self.subordinates)+"Level "+str(self.level)+"\n"
+
 
 class LevelData:
     def __init__(self, agentName):
@@ -84,16 +90,10 @@ class MilitaryHierarchy:
             self.calcLevel(a)
             if self.lvl > self.levelMap.highestLevel: self.levelMap.highestLevel = self.lvl
             self.levelMap.addLevelData(AgentName=a, Level=self.lvl)
-            #~ if self.LevelMap.has_key(self.lvl):
-                #~ self.levelMap[self.lvl] += 1
-            #~ else: self.levelMap[self.lvl] = 1
-            #~ self.hierarchy[a].setLevel(self.lvl)
+
 
     def addAgent(self, agent):
-        org = agent.get_facet_values("org_id")
-        print "agent::", agent.name, " org_id:", org
-        if org == None: return # silently? or complain of an ill-formed XML stanza? TODO: resolve this
-        rel = AgentMetadata(org, agent.name)
+        rel = AgentMetadata( agent.name)
         subs = agent.get_facet_values("subordinate_org_id")
         superiors = agent.get_facet_values("superior_org_id")
         if (len(subs) > 0): rel.addSubordinates(subs)
@@ -110,4 +110,80 @@ class MilitaryHierarchy:
         for iter in self.hierarchy.iterkeys(): 
             rtnString = rtnString +  str(self.hierarchy[iter])+"\n"
         return rtnString
+
+
+
+
+class ULHierarchy:
+	def __init__(self, uri=None, xmlString=None):
+		global doc
+		self.hierarchy = {}
+		self.levelMap = LevelMap()
+		if uri is not None:
+			doc = NonvalidatingReader.parseUri(uri)
+			self.parse()
+		self.highestLevel = 0
+		self.createLevelMap()
+		print "levelMap:", self.levelMap
+		#~ elif xmlString is not None:
+			#~ uri = 'file:bogusFile.txt' # Required by Domlette or it issues a warning.
+			#~ doc = NonvalidatingReader.parseString(xmlString, uri)
+
+	def parse(self):
+		hierarchyElement = None
+		orgName = None
+		root = doc.childNodes[0]
+		rootName = root.getAttributeNS(None, "RootID")
+		print root, 'rootName=', rootName
+		#~ print 'Society==>', society.name
+		if root.hasChildNodes():
+			orgElements = root.childNodes
+			#~ print orgElements
+			for orgElement in orgElements:
+				if orgElement.nodeName == 'Org':
+					#~ print orgElement.childNodes
+					for node in orgElement.childNodes:
+						if node.nodeName == 'OrgID':
+							#~ print 'OrgID..', node.childNodes[0].data.strip()
+							orgName = str(node.childNodes[0].data.strip())
+							hierarchyElement = AgentMetadata(orgName)
+
+						if node.nodeName == 'Rel':
+							if node.getAttributeNS(None, "Rel") == "Superior":
+								hierarchyElement.addSuperior([str(node.getAttributeNS(None, "OrgID"))])
+								#~ print  "superior org..", node.getAttributeNS(None, "OrgID")
+							if node.getAttributeNS(None, "Rel") == "Subordinate":
+								hierarchyElement.addSubordinates( [str(node.getAttributeNS(None, "OrgID"))])
+								#~ print  "subordinate org..", node.getAttributeNS(None, "OrgID")
+							self.hierarchy[orgName] = hierarchyElement
+
+	def createLevelMap(self):
+		"""
+		returns a dictionary whose keys are a level number, and whose elements are a list of agents at that level
+		"""
+		for a in self.hierarchy.iterkeys():
+			self.lvl = 0
+			self.calcLevel(a)
+			if self.lvl > self.levelMap.highestLevel: self.levelMap.highestLevel = self.lvl
+			self.levelMap.addLevelData(AgentName=a, Level=self.lvl)
+
+	def calcLevel(self, agentName):
+		if agentName is None:          return
+		self.lvl += 1
+		self.calcLevel(self.hierarchy[agentName].getSuperior())
+
+
+	def __str__(self):
+		rtnString =  "ULHierarchy==>\n"
+		for iter in self.hierarchy.iterkeys(): 
+			rtnString = rtnString +  str(self.hierarchy[iter])+"\n"
+		return rtnString
+		
+
+def runTest(uri):
+	h = ULHierarchy(uri=uri)
+	print "society...",h
+	
+if __name__ == '__main__':
+    runTest('hierarchy.xml')
 
